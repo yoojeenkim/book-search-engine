7b4bf4b156c80f3d51291404838b18b316af915f
daniel <yoojeenkim@gmail.com>
2021-11-20 16:33:08 -0500

    changed minor issues for deployment

--- server/config/connection.js
+++ server/config/connection.js
@@ -4,7 +4,6 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
-  useFindAndModify: false,
 });
 
 module.exports = mongoose.connection;
